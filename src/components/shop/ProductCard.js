650cd3ef7d5303612cadcabd785c749b02068ffe
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2023-01-17 04:26:02 +0000

    fixed the product layout issue- overflowing the div

--- src/components/shop/ProductCard.js
+++ src/components/shop/ProductCard.js
@@ -4,11 +4,14 @@ const ProductCard = props => {
 	const { title, price, category, description, image } = props;
 	return (
 		<div key={props.id} className={classes.productCard}>
-			<h3>TYPE: {category}</h3>
-			<p>Manufacturer :{title}</p>
+			<p>{title}</p>
 			<p>Price: {price}£ </p>
-			<p>description: {description}</p>
-			<img style={{ height: `100px` }} src={image} alt=""></img>
+
+			<img
+				style={{ height: `100px`, backgroundColor: `transparent` }}
+				src={image}
+				alt=""
+			></img>
 		</div>
 	);
 };
